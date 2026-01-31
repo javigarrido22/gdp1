ea46ad88d178d83579ceee76f7bdbf2a942ee7d8
javigarrido22 <jav.garridom@duocuc.cl>
2026-01-31 20:01:42 -0300

    deploy home

---
+++ .next/dev/static/chunks/gdp1_pages__error_2da965e7._.js
@@ -0,0 +1,15 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/f32c8_next_dist_compiled_7257d6cd._.js",
+  "static/chunks/f32c8_next_dist_shared_lib_985db1ff._.js",
+  "static/chunks/f32c8_next_dist_client_c2205435._.js",
+  "static/chunks/f32c8_next_dist_e2070295._.js",
+  "static/chunks/f32c8_next_error_a8996d54.js",
+  "static/chunks/[next]_entry_page-loader_ts_b77a0583._.js",
+  "static/chunks/f32c8_react-dom_80438415._.js",
+  "static/chunks/f32c8_ae72ca3f._.js",
+  "static/chunks/[root-of-the-server]__9e89c4bc._.js"
+],
+    source: "entry"
+});
